8162befdb29069deb60667dbd022e15d2827c6f4
algakbk <baktiiar.kukanov@iaau.edu.kg>
2021-01-19 12:49:35 +0600

    added todo form and it's database logic

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo, Book
 
 def homepage(request):
@@ -11,3 +11,10 @@ def test(request):
 def book(request):
     book_list = Book.objects.all()
     return render(request, "books.html", {"book_list": book_list})
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)
--- todo/urls.py
+++ todo/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import homepage, test, book
+from main.views import *
 
 
 urlpatterns = [
@@ -25,5 +25,6 @@ urlpatterns = [
     path("", homepage, name="home"),
     path("test", test, name="test"),
     path("books", book, name="books"),
+    path("add-todo/", add_todo, name="add-todo")
 ]   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
